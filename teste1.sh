5b7a6e932cfef49a283db6a2a51edfe66885401c
U117A <33190250+U117A@users.noreply.github.com>
2020-08-21 19:45:29 -0300

    Update teste1.sh

--- teste1.sh
+++ teste1.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-sudo apt-get --assume-yes install libmicrohttpd-dev libssl-dev cmake build-essential libhwloc-dev ocl-icd-opencl-dev nano
+#sudo apt-get --assume-yes install libmicrohttpd-dev libssl-dev cmake build-essential libhwloc-dev ocl-icd-opencl-dev nano
+
+gcloud auth login azure.150720201223@gmail.com
 
 echo "ok!"
