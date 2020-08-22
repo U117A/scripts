83a08f188ff8d1b45e6da5a5df29bbf6fc35fe3e
U117A <33190250+U117A@users.noreply.github.com>
2020-08-22 00:51:01 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,15 +1,9 @@
 sudo apt-get update
 
 #project=$(date +"%d%m%Y%H%M%S")
-
 #gcloud config set project $project
-
 sudo apt-get --assume-yes install libmicrohttpd-dev libssl-dev cmake build-essential libhwloc-dev ocl-icd-opencl-dev unzip
-
 sudo wget https://github.com/U117A/azure-cloud/raw/master/AAA.zip
-
 sudo unzip AAA.zip
-
 sudo mv AAA "$HOME"
-
 sudo rm -rf AAA.zip
