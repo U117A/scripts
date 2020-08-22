f2745dcd6abfc0acd1d782338afaebd898418ba5
U117A <33190250+U117A@users.noreply.github.com>
2020-08-22 01:05:21 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,6 +5,6 @@ sudo apt-get update
 sudo apt-get --assume-yes install libmicrohttpd-dev libssl-dev cmake build-essential libhwloc-dev ocl-icd-opencl-dev unzip
 sudo wget https://github.com/U117A/azure-cloud/raw/master/AAA.zip
 sudo unzip AAA.zip
-sudo rm -rf "$HOME"/AAA #IF****
+#sudo rm -rf "$HOME"/AAA #IF****
 sudo mv AAA "$HOME"
 sudo rm -rf AAA.zip
