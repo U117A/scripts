976249fb3416913ce377b38334fe90200c30f182
U117A <33190250+U117A@users.noreply.github.com>
2020-08-22 23:33:21 -0300

    Update getcodigo.sh

--- getcodigo.sh
+++ getcodigo.sh
@@ -1,7 +1,17 @@
+
 #!/bin/bash
-sleep 120
-if [ -f "codigo" ]; then
-  sudo rm -rf codigo
-if
-sudo wget https://raw.githubusercontent.com/U117A/scripts/master/codigo
-cat codigo
+
+#sleep 180
+codigo_old="$( cat codigo )"
+codigo="$codigo_old"
+
+while [ "$codigo_old" = "$codigo" ]
+do
+sudo rm -rf codigo
+sudo wget https://raw.githubusercontent.com/U117A/scripts/master/codigo --quiet
+codigo=$( cat codigo )
+sleep 5
+done
+
+echo $codigo >> codigo.hist
+echo -e "$codigo\n"
