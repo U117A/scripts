f29557105fce143b11eedd1ef103f2564734ffa9
U117A <33190250+U117A@users.noreply.github.com>
2020-08-25 23:31:42 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 export LOCAL=$(cat $(find /home -name home.file))
 cd $LOCAL
-fi
 if [ ! -e "AAA" ]; then
    sudo apt-get update
    sudo apt-get --assume-yes install build-essential cmake libuv1-dev libssl-dev libhwloc-dev git unzip
