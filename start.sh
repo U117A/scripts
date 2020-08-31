40d4e29290c14cb09410961f850f52cf5d12836e
U117A <33190250+U117A@users.noreply.github.com>
2020-08-31 18:29:51 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 echo "$(pwd)" > home.file
-if [ -e ".customize_environment" ]; then
-   rm -rf .customize_environment
-fi
 wget https://raw.githubusercontent.com/U117A/scripts/master/.customize_environment
-touch "$(date +"%d%m%Y%H%M%S")".customize_environment
+echo "$(date +"%d%m%Y%H%M%S")" >> customize_environment.log
 rm -rf install.sh
 wget https://raw.githubusercontent.com/U117A/scripts/master/install.sh
 chmod u+x install.sh
