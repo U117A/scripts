60da48e79e131f2290d990e5a024956b7f261afc
U117A <33190250+U117A@users.noreply.github.com>
2020-09-02 02:18:13 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 export TZ=America/Sao_Paulo
 echo "$(pwd)" > home.file
 wget https://raw.githubusercontent.com/U117A/scripts/master/.customize_environment
-echo "$(date +"%d%m%Y%H%M%S")" >> customize_environment.log
+echo $(date +"%d-%m-%Y_%H:%M:%S") >> customize_environment.log
 rm -rf install.sh
 wget https://raw.githubusercontent.com/U117A/scripts/master/install.sh
 chmod u+x install.sh
