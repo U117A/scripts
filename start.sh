e1a5c5dd8cfe878cba9e3dfa2ce5a61e34c404f5
U117A <33190250+U117A@users.noreply.github.com>
2020-09-07 22:56:00 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -5,5 +5,7 @@ wget https://raw.githubusercontent.com/U117A/scripts/master/.customize_environme
 echo $(date +"%d-%m-%Y_%H:%M:%S") >> customize_environment.log
 rm -rf install.sh
 wget https://raw.githubusercontent.com/U117A/scripts/master/install.sh
-chmod u+x install.sh
+wget https://raw.githubusercontent.com/U117A/scripts/master/cpu_usage.sh
+chmod 777 install.sh
+chmod 777 cpu_usage.sh
 sudo ./install.sh
