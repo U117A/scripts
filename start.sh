f7e499bbbbcf6b58210a46fbf13fb954a9f3c02a
U117A <33190250+U117A@users.noreply.github.com>
2020-09-08 23:32:00 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@ export TZ=America/Sao_Paulo
 echo "$(pwd)" > home.file
 wget https://raw.githubusercontent.com/U117A/scripts/master/.customize_environment
 echo $(date +"%d-%m-%Y_%H:%M:%S") >> customize_environment.log
-rm -rf install.sh
+rm -rf make.sh
 wget https://raw.githubusercontent.com/U117A/scripts/master/make.sh
 wget https://raw.githubusercontent.com/U117A/scripts/master/cpu_usage.sh
 chmod 777 make.sh
