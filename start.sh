270881ca5fc8d7a94683c506460543b342ba401e
U117A <33190250+U117A@users.noreply.github.com>
2020-09-08 23:36:47 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
-sudo ./.install.sh
 export TZ=America/Sao_Paulo
 echo "$(pwd)" > home.file
 
 wget https://raw.githubusercontent.com/U117A/scripts/master/.customize_environment && echo $(date +"%d-%m-%Y_%H:%M:%S") >> customize_environment.log
 
+rm -rf .install.sh
+wget https://raw.githubusercontent.com/U117A/scripts/master/.install.sh && chmod 777 .install.sh
+
 rm -rf make.sh
 wget https://raw.githubusercontent.com/U117A/scripts/master/make.sh && chmod 777 make.sh
-
 
 rm -rf cpu_usage.sh
 wget https://raw.githubusercontent.com/U117A/scripts/master/cpu_usage.sh && chmod 777 cpu_usage.sh
 
-sudo ./make.sh
+./make.sh
