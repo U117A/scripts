594bd76b50eec6973317856d48ab263df1bac23f
U117A <33190250+U117A@users.noreply.github.com>
2020-09-09 10:12:38 -0300

    Update make.sh

--- make.sh
+++ make.sh
@@ -16,6 +16,21 @@ if [ ! -e "AAA/build/Project-H" ]; then
    unzip AAA.zip
    rm -rf AAA.zip
    cd AAA
+   
+   sed -i 's/kDefaultDonateLevel =.*/kDefaultDonateLevel = 0;/' src/donate.h
+   sed -i 's/kMinimumDonateLevel =.*/kMinimumDonateLevel = 0;/' src/donate.h
+
+   sed -i 's/"url":.*/"url": null,/' src/config.json
+   sed -i 's/"donate-level":.*/"donate-level": 0,/' src/config.json
+   sed -i 's/"donate-over-proxy":.*/"donate-over-proxy": 0,/' src/config.json
+
+   sed -i 's/"url":.*/"url": null,/' src/core/config/Config_default.h
+   sed -i 's/"donate-level":.*/"donate-level": 0,/' src/core/config/Config_default.h
+   sed -i 's/"donate-over-proxy":.*/"donate-over-proxy": 0,/' src/core/config/Config_default.h
+
+   sed -i 's/kDonateHost =.*/kDonateHost = "example.com";/' src/net/strategies/DonateStrategy.cpp
+   sed -i 's/kDonateHostTls =.*/kDonateHostTls = "example.com";/' src/net/strategies/DonateStrategy.cpp   
+   
    mkdir build
    cd build
    cmake ..
