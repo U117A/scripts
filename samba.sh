a5cdec457eb04afe40b6979d9a72f7d20dbd5145
U117A <33190250+U117A@users.noreply.github.com>
2020-09-11 23:41:16 -0300

    Update samba.sh

--- samba.sh
+++ samba.sh
@@ -3,14 +3,6 @@
 sudo apt install samba
 chmod 777 /home/
 
-[publico]
-path = /home/
-public = yes
-writable = yes
-comment = smb share
-printable = no
-guest ok = yes
-
->> /etc/samba/smb.conf
+sudo ./teste >> /etc/samba/smb.conf
 
 sudo /etc/init.d/samba-ad-dc restart
