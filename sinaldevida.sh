42f6ed6927517e895630f90c387ff6413bc05132
U117A <33190250+U117A@users.noreply.github.com>
2020-10-12 16:40:30 -0300

    Update sinaldevida.sh

--- sinaldevida.sh
+++ sinaldevida.sh
@@ -1,12 +1,43 @@
-
 #!/bin/bash
-while [ 1=1 ] 
+
+export TZ=America/Sao_Paulo
+
+padrao="\033[0m"
+verde="\033[0;32m"
+
+command=""
+#command=" ${command} [ ! -e 'AAA/build/Project-H' ] && rm -rf start.* && wget https://raw.githubusercontent.com/U117A/scripts/master/start.sh && "
+#command=" ${command} chmod 777 start.sh && "
+#command=" ${command} ./start.sh >> start.log ; "
+command=" ${command} echo -e '"${verde}"'ON  "'$(ps -o etime= -C "Project-H")'" '"${padrao}"' ; "
+command=" ${command} echo "'$(date +"%d%m%Y%H%M%S") >> log.tmux'" ; "
+command=" ${command} sleep 1 ; exit"
+
+#echo -e "$verde" "$command" "$padrao"
+
+while true
 do
- sleep 5
-#gcloud auth login robo-567@smart-display-287302.iam.gserviceaccount.com --quiet
-#gcloud config set account azure.220720202126@gmail.com
- gcloud alpha cloud-shell ssh --boosted --command='sudo rm -rf *.ts ; touch $(date +"%d%m%Y%H%M%S").ts' --project p$(date +"%d%m%Y%H%M%S") --quiet 
-# logout
- sudo rm -rf *.ts 
- touch $(date +"%d%m%Y%H%M%S").ts
+
+ sudo rm -rf .ssh/known_hosts
+
+ echo
+ echo 'Lendo lista de contas...'
+ gcloud auth list --format="value(account)" > contas.txt
+ lista=( `cat "contas.txt" `)
+
+ for conta in "${lista[@]}"
+  do
+
+  if echo "$conta" | grep -q "$1"; then
+
+     echo
+     echo $conta
+     echo "$(date)"
+     #gcloud config set account $conta
+     gcloud beta cloud-shell ssh --boosted --command="$command" --account=$conta --authorize-session --quiet
+
+  fi
+
+ done
+
 done
