a15542016e450f07170bed68f97f8e82e356ce25
U117A <33190250+U117A@users.noreply.github.com>
2020-11-22 16:30:52 -0300

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,9 +1,13 @@
 #!/data/data/com.termux/files/usr/bin/bash
 
-id=(`cat done`)
+id=2
+
+done=(`cat done`)
 
 echo $id
 
-if [ $id <> 1 ]; then
-   echo "diferente"
+if [ $id != $done ]; then
+   echo "update"
+else
+   echo "bypass"
 fi
