9cb986983177363b3e7c5e82a4160d69b45a1d2c
U117A <33190250+U117A@users.noreply.github.com>
2020-11-22 01:41:44 -0300

    Update while.sh

--- while.sh
+++ while.sh
@@ -2,6 +2,7 @@
 
 while true
 do
-wget -q -N https://raw.githubusercontent.com/U117A/scripts/master/config.sh && chmod 777 && ./config.sh && date >> while.log
+wget -q -N https://raw.githubusercontent.com/U117A/scripts/master/config.sh && chmod 777 && ./config.sh
+date >> while.log
 sleep 10
 done
