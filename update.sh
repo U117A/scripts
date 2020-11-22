45f29d2b5d081fb09c45f2c0b0a57fa60111a9df
U117A <33190250+U117A@users.noreply.github.com>
2020-11-22 17:57:01 -0300

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
 #!/data/data/com.termux/files/usr/bin/bash
 
-id=2
+id=3
 
 done=(`cat done`)
 
@@ -8,6 +8,7 @@ echo $id
 
 if [ $id != $done ]; then
    echo "update"
+   pkg install uuid
    echo $id > done
 else
    echo "bypass"
