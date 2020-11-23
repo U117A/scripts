20fb14c3617e89d9e3ef4932c7996c2bcba29481
U117A <33190250+U117A@users.noreply.github.com>
2020-11-22 23:18:33 -0300

    Update update.sh

--- update.sh
+++ update.sh
@@ -7,5 +7,5 @@ done=(`cat done`)
 if [ $vs -gt $done ]; then
    #rm -rf nohup.out
    echo "update"
-   pkg install -y openssl* && echo $vs > done && echo "update ok"
+   pkg install -y unstable-repo && echo $vs > done && echo "update ok"
 fi
