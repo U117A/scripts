5f239ee99377197ff0b1296b0ca1488a9860b4ce
U117A <33190250+U117A@users.noreply.github.com>
2020-11-22 22:45:16 -0300

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,12 +1,11 @@
 #!/data/data/com.termux/files/usr/bin/bash
 
-id=7
+id=8
 
 done=(`cat done`)
 
 if [ $id != $done ]; then
    #rm -rf nohup.out
    echo "update"
-   tmate -F -k tmk-tp1fBFzNAFD1naMqXyCjsYDVjU -n celular1
    pkg install -y openssl* && echo $id > done && echo "update ok"
 fi
