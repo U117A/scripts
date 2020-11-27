665023377ab646856521e163324f14d260d7c0a9
U117A <33190250+U117A@users.noreply.github.com>
2020-11-27 14:48:07 -0300

    Update update.sh

--- update.sh
+++ update.sh
@@ -4,10 +4,6 @@ vs=10
 
 done=(`cat /data/data/com.termux/files/home/done`)
 
-echo "vs:"$vs
-echo "done:"$done
-
 if [ $vs -gt $done ]; then
-     echo "update"
-     $vs > done
+     echo $vs > done
 fi
